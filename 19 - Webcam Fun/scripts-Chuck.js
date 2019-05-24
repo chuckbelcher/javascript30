5605cd838af6c80366b8e2be5dc5315cc4fb8f62
Chuck Belcher <cfbelcher@pateogroup.com>
2019-05-23 22:13:44 -0400

    initial setup for lecture 19

---
+++ 19 - Webcam Fun/scripts-Chuck.js
@@ -0,0 +1,24 @@
+//Declare Variables
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+        });
+}
+
+
+//Take Picture
+function takePhoto() {
+    console.log('smile');
+}
+
+
+getVideo();
+
+//video at 6:33
